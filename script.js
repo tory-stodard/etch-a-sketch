3fa85d4298f8e2d834cd66502e541eee70d7cea5
Tory Stodard <tstodard26@gmail.com>
2022-03-29 01:19:53 -0600

    logic for grid generation complete.

--- script.js
+++ script.js
@@ -1,42 +1,38 @@
-let gridSize = 256;
-const clearGridBtn = document.querySelector(".clearGrid");
-const changeGridBtn = document.querySelector(".changeGrid");
+let rowAndColumnSize = 16;
 
-clearGridBtn.addEventListener("click", clearGrid);
-changeGridBtn.addEventListener("click", changeGridSize);
+const gridSizeBtn = document.querySelector(".gridSizeBtn");
+const resetBtn = document.querySelector(".resetBtn");
+const showHideGridBtn = document.querySelector(".showHideGrid");
+
+gridSizeBtn.addEventListener("click", changeGridSize);
+resetBtn.addEventListener("click", clearGrid);
+showHideGridBtn.addEventListener("click", showHideGrid);
 
 createGrid();
 
 function createGrid() {
-  const gridContainer = document.querySelector(".gridContainer");
+  const grid = document.querySelector(".grid");
+  let gridSize = rowAndColumnSize * rowAndColumnSize;
+
+  grid.style.gridTemplateColumns = `repeat(${rowAndColumnSize},1fr)`;
+  grid.style.gridTemplateRows = `repeat(${rowAndColumnSize},1fr)`;
 
   for (let i = 0; i < gridSize; i++) {
-    const box = document.createElement("div");
-    box.classList.add("box");
-    gridContainer.appendChild(box);
+    const pixel = document.createElement("div");
+    pixel.classList.add("pixel");
+    grid.appendChild(pixel);
   }
 }
 
 function changeGridSize() {
-  // const gridContainer = document.querySelector(".gridContainer");
-  // let gtc;
-  // let gtr;
-
-  clearGrid();
-  gridSize = prompt("Choose a grid size (1-100).");
-  gridSize *= gridSize;
-  // gtc = gridSize / gridSize;
-  // gtc = "repeat(" + gtc.toString() + ",1fr)";
-  // gtr = gridSize / gridSize;
-  // gtr = "repeat(" + gtr.toString() + ",1fr";
-  // gridContainer.style.gridTemplateColumns = gtc;
-  // gridContainer.style.gridTemplateRows = gtr;
+  rowAndColumnSize = prompt("Enter a number between 1 and 100.");
   createGrid();
 }
 
 function clearGrid() {
-  for (let i = 0; i < gridSize; i++) {
-    const box = document.querySelector(".box");
-    box.remove();
-  }
+  //clear the grid.
+}
+
+function showHideGrid() {
+  //show or hide grid.
 }
